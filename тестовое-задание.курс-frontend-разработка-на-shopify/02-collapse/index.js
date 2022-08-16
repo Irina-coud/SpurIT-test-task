04e17eead00a38e0736b0ab4141b2d14a94ba156
Irina-coud <irinatobolich313@gmail.com>
2022-08-16 19:43:37 +0300

    feat: added finished second task

--- тестовое-задание.курс-frontend-разработка-на-shopify/02-collapse/index.js
+++ тестовое-задание.курс-frontend-разработка-на-shopify/02-collapse/index.js
@@ -1,1 +1,52 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const button = document.querySelector(".collapsible__button");
+const buttonVisibleContent = document.querySelector(
+  ".collapsible__action--visible"
+);
+const buttonHiddenContent = document.querySelector(
+  ".collapsible__action--hidden"
+);
+const content = document.querySelector(".collapsible__content");
+
+let isOpen = false;
+
+const visibleEffect = [
+  {
+    opacity: 0,
+    transform: "translateY(-50px)",
+  },
+  {
+    opacity: 1,
+    transform: "translateY(0px)",
+  },
+];
+
+const hiddenEffect = [
+  {
+    opacity: 1,
+  },
+  {
+    opacity: 0,
+  },
+];
+
+const timing = {
+  duration: 300,
+  fill: "forwards",
+};
+
+content.animate(visibleEffect, timing).pause();
+buttonVisibleContent.animate(visibleEffect, timing).pause();
+
+button.onclick = () => {
+  if (isOpen) {
+    content.animate(hiddenEffect, timing);
+    buttonVisibleContent.animate(hiddenEffect, timing);
+    buttonHiddenContent.animate(visibleEffect, timing);
+    isOpen = false;
+  } else {
+    content.animate(visibleEffect, timing);
+    buttonVisibleContent.animate(visibleEffect, timing);
+    buttonHiddenContent.animate(hiddenEffect, timing);
+    isOpen = true;
+  }
+};
